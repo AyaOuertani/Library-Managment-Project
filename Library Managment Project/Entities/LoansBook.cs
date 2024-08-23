b4571af773028ea8cb26291474369212638b55ed
Aya Ouertani <ouertaniaya03@gmail.com>
2024-08-23 18:08:30 +0100

    Changing types

--- Library Managment Project/Entities/LoansBook.cs
+++ Library Managment Project/Entities/LoansBook.cs
@@ -4,12 +4,13 @@ namespace Library_Managment_Project.Entities
 {
     public class LoansBook
     {
+        public string LoanID { get; set; }
         public DateTime  DateOfLoan { get; set; }
         public DateTime DateOfReturn { get; set; }
         public StatusOfLoans LoanStatus { get; set; }
-        public int BookId { get; set; }
+        public string BookId { get; set; }
         public Book? Book { get; set; }
-        public int MemberId { get; set; }
+        public string MemberId { get; set; }
         public Member? Member { get; set; }
     }
 }
